e2cfbfc289e76b9249f2f6844d33b1bea63ffe5c
Magesh Kuppan <tkmagesh77@gmail.com>
2019-09-19 14:10:07 +0800

    web server complete

--- 05-web-server/server.js
+++ 05-web-server/server.js
@@ -1,4 +1,7 @@
-const http = require('http');
+const http = require('http'),
+	fs = require('fs'),
+	path = require('path'),
+	url = require('url');
 
 /*
 req - IncomingMessage (class) - Readable Stream
@@ -6,9 +9,19 @@ res - ServerResponse (class) - Writable Stream
 */
 
 const server = http.createServer((req, res) => {
-	console.log('a new connection is established');
-	res.write('<h1>Welcome to node.js</h1>');
-	res.end();
+	
+	const parsedUrl = url.parse(req.url); 
+	console.log(`${req.method}\t${parsedUrl.pathname}`);
+	const resourceName = parsedUrl.pathname,
+		resourceFullName = path.join(__dirname, resourceName);
+
+	if (!fs.existsSync(resourceFullName)){
+		res.statusCode = 404;
+		res.end();
+		return;
+	}
+	const stream = fs.createReadStream(resourceFullName);
+	stream.pipe(res);
 });
 
 server.listen(8080);
